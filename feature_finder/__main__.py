f141e40504076555dd1b4d241be61342728a8e08
Catherine Noll <noll.catherine@gmail.com>
2018-05-24 11:23:16 +0200

    Add function to apply plugins, and misc. cleanup.

--- feature_finder/__main__.py
+++ feature_finder/__main__.py
@@ -1,6 +1,6 @@
 import argparse
 
-from feature_finder.find_features import Model, setup_model
+from feature_finder.find_features import Model
 
 
 def main():
@@ -9,22 +9,21 @@ def main():
     parser.add_argument('data', help=('Data in CSV format.'))
     parser.add_argument(
         '--plugins',
-        help='Activate plugin(s) defined in plugins directory',
+        help='Use plugin(s) defined in plugins directory to add or clean up feature columns.',
         action='store_true'
     )
     args = parser.parse_args()
+    model_type = args.model
     data_csv = args.data
     plugins = args.plugins
 
     if not data_csv:
-        print("Please provide the path to your data (in CSV format).")
+        print("Please provide the path to your data (CSV).")
         exit
 
-    data = setup_model(data_csv)
-
-    selectors = Model(plugins)
-    selectors.select(data)
-    selectors.print_results()
+    model = Model(model_type, plugins)
+    model.select(data_csv)
+    model.print_results()
 
 
 if __name__ == "__main__":
--- feature_finder/find_features.py
+++ feature_finder/find_features.py
@@ -1,25 +1,47 @@
 #!/usr/bin/env python
 
 import csv
-# import inspect
-# from operator import attrgetter
+import inspect
+
+import pandas as pd
+from sklearn.linear_model import LinearRegression, LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold
 
 from feature_finder.plugins import plugins
 
 
 class Model:
+    models = {'linear': LinearRegression, 'logistic': LogisticRegression}
 
-    def __init__(self):
-        pass
+    def __init__(self, model_type, plugins=None):
+        if model_type not in self.models:
+            raise TypeError(
+                "Please specify one of the following model types: {}."
+                .format(', '.join(self.models.keys())))
+        self.model = self.models[model_type]
+        self.plugins = plugins or []
 
-    def select(data):
+    def select(self, data):
         """Get the result of fitting the data on selectors."""
+        if self.plugins:
+            data = self.customize(data)
         raise NotImplementedError
+
+    def customize(self, data):
+        """Apply plugins."""
+        plugins_functions = [
+            obj for name, obj in inspect.getmembers(plugins)
+            if inspect.isfunction(obj)
+        ]
+        for fn in plugins_functions:
+            try:
+                data = fn(data)
+            except Exception:
+                raise plugins.PluginException(
+                    'Error with plugin "{}".'.format(fn.__name__))
+        return data
 
     def print_results(self):
         """Prints the model's results."""
         raise NotImplementedError
-
-
-def setup_model(data_csv):
-    raise NotImplementedError
