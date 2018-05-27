ad4f88a5421e80a2140e1d15573ee0001b259b46
Catherine Noll <noll.catherine@gmail.com>
2018-05-27 23:14:29 +0200

    Update test skeleton.

--- feature_finder/tests/test_find_features.py
+++ feature_finder/tests/test_find_features.py
@@ -1,6 +1,8 @@
 import os
 from unittest import TestCase
 
+from feature_finder.find_features import Model, setup_data
+
 
 class TestFindFeatures(TestCase):
 
@@ -8,10 +10,18 @@ class TestFindFeatures(TestCase):
         self.dir_path = os.path.dirname(os.path.realpath(__file__))
         self.sample_data_csv = None
 
+    def test_validate(self):
+        """`validate_data` raises exceptions on errors in input format."""
+        raise NotImplementedError
+
+    def test_setup_data(self):
+        """`setup_data` returns a dataframe and y-column name in the dataframe."""
+        raise NotImplementedError
+
     def test_linear_regression(self):
         """Linear regression model selects most predictive features."""
-       raise NotImplementedError
+        raise NotImplementedError
 
-   def test_logistic_regression(self):
+    def test_logistic_regression(self):
         """Logistic regression model selects most predictive features."""
-       raise NotImplementedError
+        raise NotImplementedError
